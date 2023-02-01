df31190bd88ba471cb57f0ea4e16784c94ca278f
Waferkrim <124156866+Waferkrim@users.noreply.github.com>
2023-02-02 02:22:41 +0700

    first commit

--- Text-Creator.js
+++ Text-Creator.js
@@ -3,6 +3,6 @@ function popupku() {
   var pWm = document.createElement("a");
   pWm.setAttribute("href", "https://wa.me/6283805005602");
   pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
-  pWm.innerHTML = "Code By : krfan Febriansyah";
+  pWm.innerHTML = "Code By : Irfan Febriansyah";
   body.appendChild(pWm);
 }
