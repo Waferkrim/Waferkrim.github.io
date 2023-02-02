c5c1d5d7fbbe263b74c3611e33cf60f4cc47356f
Waferkrim <124156866+Waferkrim@users.noreply.github.com>
2023-02-02 22:09:24 +0700

    first commit

---
+++ Text-Creator-2.js
@@ -0,0 +1,1 @@
+function popupku() { var body = document.querySelector("body"); var pWm = document.createElement("a"); pWm.setAttribute("href", "https://wa.me/62895321907807"); pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px"); pWm.innerHTML = "Code By : Ananda"; body.appendChild(pWm);}
